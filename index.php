47995f59f7fcac8359835f010be35326284843ea
Matt <matthew.behn@gmail.com>
2014-02-10 13:08:37 -0600

    changes

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+
+        "http://www.w3.org/TR/html4/loose.dtd">
+
+
+
+<html lang="en" itemscope itemtype="http://schema.org/LocalBusiness">
+
+
+
+<head>
+
+
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+	<link rel='shortcut icon' href='favicon.jpg' /><link rel='icon' href='http://www.zerogravityinstitute.com/favicon.jpg' />
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700italic,900italic' rel='stylesheet' type='text/css'>
+
+	<title itemprop="name">Zero Gravity Institute &reg;</title>
+
+
+
+</head>
+
+
+
+<body>
+
+<div id="page-wrapper">
+	<div id="container">
+	<div id="header">
+		<div id="main-banner">
+			<a href="http://mbehn.com/zginstitute"><h1 id="page-title"><span id="zero">Zero</span><span id="gravity">Gravity</span><span id="institute">Institute</span></h1></a>
+			<div id="address-container">
+				<div id="address" itemprop="address" itemscope itemtype="http://schema.org/Address">
+					<li id="maps-popout">open in google maps <span id="map-icon"><img src="assets/images/mapicon.png"></span></li>
+					<div id="bottom-address"><li id="map" onmouseover="mapsPopout()" onmouseout="popoutHide()"><a itemprop="maps" href="https://www.google.com/maps/preview/place/ZERO+GRAVITY+INSTITUTE/@30.2421676,-97.7816807,17z/data=!3m1!4b1!4m2!3m1!1s0x0:0x9989b23cf188e3fd" target="_blank"><span itemprop="streetAddress">2919 Manchaca Rd Suite 105A</span></a></li>
+										<li><span itemprop="addressLocality">Austin</span>, <span itemprop="addressRegion">TX</span> <span itemprop="postalCode">78704</span></li>
+										<li><span itemprop="telephone">512-707-1191</span></li><li itemprop="url" id="url">http://mbehn.com/zginstitute/</li></div>
+				</div>
+			</div>
+		</div>
+		<div id="navigation">
+			<nav>
+				<ul>
+					<li><a href="#">intro to floating</a></li>
+					<li><a href="#">book an appointment</a></li>
+					<li><a href="#">rates & discounts</a></li>
+					<li><a href="#">media gallery</a></li>
+					<li><a href="#">before you float</a></li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+	<div id="main-content">
+	</div>
+	<div id="footer">
+	</div>
+
+</div></div>
+<script type="text/javascript">
+</script>
+<script type="text/javascript" src="script.js"></script>
+<script type="text/javascript">
+var mapsPopout = function() {
+	document.getElementById("maps-popout").style.opacity="1";
+}
+
+var popoutHide = function(){
+	document.getElementById("maps-popout").style.opacity="0";
+}
+</script>
+</body>
+
+
+
+</html>
+
+

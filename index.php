8e0830f363c929df407b50d88279362d99069f6d
Matt <matthew.behn@gmail.com>
2014-02-17 15:45:01 -0600

    quick change, made slider js external file

--- index.php
+++ index.php
@@ -85,51 +85,9 @@ var popoutHide = function(){
 	document.getElementById("maps-popout").style.opacity="0";
 }
 
-var slider = {
-	setInitialImage : function() {
-		var images = document.getElementsByClassName("slider-images");
-		images[0].style.opacity="1";
-	},
-	images : document.getElementsByClassName("slider-images"),
-	startSlider : setTimeout(function() {
-			for (var i = 0;i<slider.images.length;i++) {
-				currentImage = slider.images[i];
-				if (i===0) {
-					currentImage.style.opacity="1";
-				} else if (i===1) {
-					slider.images[0].opacity="0";
-					slider.images[i].opacity="1";
-				} 
-			}
-		},4000),
-	showNav :  function() {
-		document.getElementById("slide-left").style.opacity="1";
-		document.getElementById("slide-right").style.opacity="1";
-		document.getElementById("slide-left").style.left="3px";
-		document.getElementById("slide-right").style.right="3px";
-
-	},
-	hideNav : function() {
-		document.getElementById("slide-left").style.opacity="0";
-		document.getElementById("slide-right").style.opacity="0";
-		document.getElementById("slide-left").style.left="10px";
-		document.getElementById("slide-right").style.right="10px";
-	}
-
-}
-
-slider.setInitialImage();
-slider.startSlider;
-var navs = document.getElementById("slider");
-navs.onmouseover = function() {
-	slider.showNav();
-}
-
-navs.onmouseout = function() {
-	slider.hideNav();
-}
-
 </script>
+
+<script type="text/javascript" src="slider.js"></script>
 
 </body>
 
